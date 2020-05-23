139534b9ef0342f2514f72bb0428c54f59423fc8
Euzebio <euzebiosantanawork@gmail.com>
2020-05-23 15:22:40 -0300

    version1.0

---
+++ aula6.1_logica.js
@@ -0,0 +1,4 @@
+console.log ("Estou escrevendo primeira linha")
+console.log ("Estou escrevendo segunda linha")
+console.log ("Se usar; consigo colocar dois comandos\
+apos o outro"); console.log ("Estou esvrevendo")
