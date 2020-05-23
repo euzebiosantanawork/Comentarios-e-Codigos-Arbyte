be3a320cdbfc61fc64a2813cef4fded55ef90090
Euzebio <euzebiosantanawork@gmail.com>
2020-05-23 15:20:09 -0300

    version1.0

---
+++ aula4_SEO.js
@@ -0,0 +1,110 @@
+/*
+SEO - Search Engine Optimization
+
+Em resumo, e a arte de influenciar de 
+algua maineira os algoritimos de raqueamento dos mecanismos 
+de busca, levando nosso resultado a melhores posicoes.
+
+ENTENDA A DIFERANCA ENTRE
+Trafego Organico X Trafego Pago
+
+Trafego Pago, fica em cima da pagina
+Trafego Organico. fica no meio da pagina
+
+QUANTO CUSTA UM AD?
+ na mecanica com que o Google construiu o AdSense, os termos
+de busca sao leiolodados. Seu custo e expresso em CPC 
+(Custo por Clique)
+
+COMO O GOOGLE ORDENA
+ Larry Page e Sergei Bryan criaram o algoritimo ha 
+decadas, e esse algoritimo evolui muito desde entao.
+ Ele leva muitos fatores em conta, e pouquissimos times entao
+possuem permissao para mexer nele.
+ Sao mais de 4k de de variavis guardadas a 7 chaves. Nem os 
+engenheiros do proprio google sabem todas elas
+
+KIT DE SOBREVIVENCIA NO SEO
+alguns poucos aunico do google + ferramentas de SEO 
++ teste empiricos + bom sensso
+
+PAGERANK - FATORES: INFRA
+- Velocidades de carregamento
+- https:// (paginas criptografadas)
+
+PAGERANK - FATORES: SINTAXE
+- title, h1, h2, h3
+- meta tags
+- hml seguido os padroes w3c
+https://w3.org/standarsds/
+- responsive
+http:/ami.responsivedesign.is
+
+Como o Bootstrap, nosso site sai responsivo 
+de largada
+
+PAGERANK - FATORES: off-page
+-robots.txt
+-sitemap.xml
+-schema.org
+-backlinks
+
+ Um jeito de todos voces subirem no rankeamento do Google e 
+atraves de rerenciamento por paginas relevantes. Vamos linkar
+as paginas de voces no nosso site, portanto passem-nos seus 
+github.io
+
+PGAERANK - FAOTRES: SEMANTICA
+- Correlacao de sentido entre o title e as metas tags com 
+o conteudo da pagina (texto, imagens e videos)
+- Propocao de imagem e texto
+
+FERRAMENTAS DE AUXILIO
+    - Google analytics
+    Google Analytics lets you measure your adverstising ROI well
+    as track your Flash, video and social networking sites and 
+    applications.
+    - Google pagesspeed
+    PageSpeed Insights analyzes the content of a web page and
+    then generates suggestions to make it faster.
+    - Chrome Devtools
+    Chrome BevTools is a set of web developer tooles built 
+    directly into the Google Chrome browser. Dev Tools can help 
+    you edit pages on-the-fly and diagnose problems quickly, 
+    wich ultimately helps you build better websites, faster.
+    - Hotjar
+    Hotjar allows you to visualize how users engage with your
+    site. Hotjar uses interactive heatmaps of their clicks 
+    and actions, recordings, of their sessions, and gathering 
+    of their words from survey and feedback polls to help you
+    build a strong, data-backed uderstandig from survey and 
+    feedback polls to help you build a strong, data-backed
+    uderstanding of what exactly people are using your site, 
+    for, and how theyre using it.
+     
+    - Google Console
+    Manege and get insights into everthig that powers your 
+    cloud application - including web applications, 
+    data analysis, virtual machines, datastores, databases,
+    networking, and developer services. Cloud Console helps
+    you deploy, scale, and diagnose production issues in a
+    simple web-based interface. Search to quickly find 
+    resources and cnnect to instances via SSh in the browser.
+    Nadle DevOps workflows on the go with powerful native iOS
+    ans andorid applications. master the most complex 
+    development tasks with Cloud Shell, your admin machine in
+    the cloud.
+    - Facebook Pixeis
+    The Facebook pixel is code that you place on your website.
+    It collects data that helps you track conversions from 
+    Facebook ads, build targeted audences for future ads, and 
+    remarket to people who have already taken some kind of 
+    action on your website.
+GOOGLE ANALYTICS
+ Google analytics e um servico gratuito e oferecido pelo Google.
+Com apenas um codigo inserido na pagina cadastrada, estatisticas
+de visitacao sao enviadas ao sistema e apresentadas ao dono do 
+site.
+GOOGLE PAGESPEED
+ google analytics e um servico gratuito do Google. 
+Nos ajuda a otimizar a velocidade de carregamento da pagina.
