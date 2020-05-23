26d2b1f7afeccca693adaf09c22d0f30eb2a9d76
Euzebio <euzebiosantanawork@gmail.com>
2020-05-23 15:31:56 -0300

    version1.0

---
+++ aula11.6_arrays.js
@@ -0,0 +1,9 @@
+var string_multi_linha = "ola\
+venho de um lugar muito longe escrever\
+Esse texto erno para dizer que\
+barras podem fazer minha string\
+continua crescendo porem\
+as aspas podem dar fim a minha\
+pequena byteVida"
+
+console.log(string_multi_linha)
